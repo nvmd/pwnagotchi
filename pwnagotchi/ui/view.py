790b19659e6e4b55b7591b42489a13cb90db02a1
Sergey Kazenyuk <kazenyuk@pm.me>
2025-01-01 07:19:09 +0300

    view: fix typo

--- pwnagotchi/ui/view.py
+++ pwnagotchi/ui/view.py
@@ -113,7 +113,7 @@ class View(object):
         self._state.has_element(key)
 
     def add_element(self, key, elem):
-        if self.invert is 1 and elem.color:
+        if self.invert == 1 and elem.color:
             if elem.color == 0xff:
                 elem.color = 0x00
             elif elem.color == 0x00:
