257ef6734974d4cd54e29dda76a44461ce91832b
Sergey Kazenyuk <kazenyuk@pm.me>
2025-01-12 08:36:18 +0300

    flake: overlays to fix stable-baselines2 and ale-py on darwin

--- flake.nix
+++ flake.nix
@@ -9,7 +9,56 @@
   outputs = { self, flake-utils, nixpkgs, ... }@inputs:
 
      flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (self: super: {
+
+            pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
+              (python-self: python-super: {
+
+                stable-baselines3 = python-super.stable-baselines3.overridePythonAttrs (oldAttrs: {
+                  disabledTestPaths = oldAttrs.disabledTestPaths ++ super.lib.optional super.stdenv.isDarwin [
+                      "tests/test_logger.py"  # darwin: Trace/BPT trap: 5
+                  ];
+                  # disabledTests = [
+                  #   # Upstream
+                  #   # Tests that attempt to access the filesystem
+                  #   "test_make_atari_env"
+                  #   "test_vec_env_monitor_kwargs"
+                  # ];
+                });
+
+                # needed by stable-baselines3, shimmy
+                ale-py = python-super.ale-py.overridePythonAttrs (oldAttrs: {
+
+                  # To propagate `cmakeFlags`
+                  # inject `CMAKE_ARGS` into setuptools' invocation of cmake
+                  # Shell expansions won't be performed, use `preConfigure` to 
+                  # prepare flags that rely on it
+                  patchPhase = ''
+                    substituteInPlace setup.py \
+                      --replace-fail "cmake_args = [" \
+                      "cmake_args = os.environ.get(\"CMAKE_ARGS\", \"\").split() + ["
+                  '';
+
+                  preConfigure = ''
+                    export CMAKE_ARGS=$cmakeFlags "''${cmakeFlagsArray[@]}"
+                  '';
+
+                  cmakeFlags = super.lib.optional super.stdenv.isDarwin [
+                    "-DCMAKE_CXX_COMPILER_AR=${super.stdenv.cc}/bin/ar"
+                    "-DCMAKE_CXX_COMPILER_RANLIB=${super.stdenv.cc}/bin/ranlib"
+                  ];
+
+                  meta.broken = false;
+                });
+              })
+            ];
+
+          })
+        ];
+      };
       forPython = pkgs.python311;
       pythonWithPackages = forPython.withPackages(ps: with ps; [
             pip
